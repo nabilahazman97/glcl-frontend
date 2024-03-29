733d458070035b187c7598bcf1096a6224a65ee0
badz20 <badrulz2000@gmail.com>
2024-03-29 13:13:27 +0800

    update

--- src/pages/Tables/DatatableTables.js
+++ src/pages/Tables/DatatableTables.js
@@ -1,6 +1,7 @@
 // src/components/filter.
 import React, { useMemo } from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 //import components
 import Breadcrumbs from '../../components/Common/Breadcrumb';
@@ -34,7 +35,7 @@ function DatatableTables() {
                 accessor: 'actions',
                 Cell: ({ row }) => (
                     <div className="d-flex flex-wrap gap-2 justify-content-center">
-                        <button
+                        {/* <button
                             type="button"
                             className="btn btn-success approveBtn"
                         >
@@ -48,7 +49,17 @@ function DatatableTables() {
                         >
                             <i className="bx bx-x-circle font-size-16 align-middle me-1"></i>{" "}
                             Reject
-                        </button>
+                        </button> */}
+                        <Link to="/member-approval" style={{ textDecoration: 'none' }}>
+                            <button
+                                type="button"
+                                className="btn btn-primary rejectBtn"
+                            >
+                                <i className="mdi mdi-eye-outline font-size-16 align-middle me-1"></i>{" "}
+                                View
+                            </button>
+                        </Link>
+
                     </div>
                 )
             },
@@ -69,6 +80,7 @@ function DatatableTables() {
             "age": 38,
             "phonenum": "012-1254646",
         },
+
 
 
     ];
---
+++ src/pages/Tables/MemberProfileList.js
@@ -0,0 +1,95 @@
+// src/components/filter.
+import React, { useMemo } from "react";
+import PropTypes from 'prop-types';
+
+//import components
+import Breadcrumbs from '../../components/Common/Breadcrumb';
+import TableContainer from '../../components/Common/TableContainer';
+import { Button } from 'reactstrap';
+
+import './datatables.scss';
+
+function DatatableTables() {
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'Name',
+                accessor: 'name',
+            },
+            {
+                Header: 'NRIC No.',
+                accessor: 'icNum'
+            },
+
+            {
+                Header: 'Membership ID',
+                accessor: 'memberid'
+            },
+            {
+                Header: 'Email Address',
+                accessor: 'email'
+            },
+            {
+                Header: 'Actions',
+                accessor: 'actions',
+                Cell: ({ row }) => (
+                    <div className="d-flex flex-wrap gap-2 justify-content-center">
+                        <button
+                            type="button"
+                            className="btn btn-danger rejectBtn"
+                        >
+                            <i className="bx bx-trash font-size-16 align-middle me-1"></i>{" "}
+                            Remove
+                        </button>
+                    </div>
+                )
+            },
+        ],
+        []
+    );
+
+    const data = [
+        {
+            "name": "Tony Lee",
+            "icNum": "873939-01-0389",
+            "memberid": "2",
+            "email": "tony@gmail.com",
+        },
+        {
+            "name": "Sarah J",
+            "icNum": "83939-01-0378",
+            "memberid": "1",
+            "email": "sarah@gmail.com",
+        },
+        
+
+
+    ];
+
+    //meta title
+    document.title = "GLCL";
+
+    return (
+        <div className="page-content">
+            <div className="container-fluid">
+                <Breadcrumbs title="Tables" breadcrumbItem="LIST OF MEMBERS" />
+                {/* <Table columns={columns} data={data} /> */}
+                <TableContainer
+                    columns={columns}
+                    data={data}
+                    isGlobalFilter={true}
+                    isAddOptions={false}
+                    customPageSize={10}
+                    className="custom-header-css"
+                />
+            </div>
+        </div>
+    );
+}
+DatatableTables.propTypes = {
+    preGlobalFilteredRows: PropTypes.any,
+
+};
+
+
+export default DatatableTables;
